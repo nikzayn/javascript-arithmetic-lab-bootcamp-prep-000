279c0cf8a4628e83437513bd57b5fa4a86b61685
NIKHIL VAIDYAR <nikhilvaidyar1997@gmail.com>
2018-01-28 20:54:05 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a,b){
+ var result = a + b
+  return result
+}
+add()
+
+function subtract(a,b){
+  var result = a - b
+  return result
+}
+subtract()
+
+function multiply(a,b){
+  var result = a * b
+  return result
+}
+multiply()
+
+function divide(a,b){
+  var result = a / b
+  return result
+}
+divide()
+
+function inc(n){
+  n++
+  return n
+}
+function dec(n){
+  n--
+  return n
+}
+function makeInt(n){
+  parseInt('0a', 10);
+}
+function preserveDecimal(n){
+  parseInt('2.22222')
+}
+
